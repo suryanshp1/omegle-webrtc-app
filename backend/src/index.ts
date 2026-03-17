4d906a12595c74abc8762733ad75b1760c25261b
suryanshp1 <suryanshp1@gmail.com>
2026-03-17 19:56:25 +0530

    feat: init backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,16 @@
-
+import express from "express";
+import http from "http";
 import { Server } from "socket.io";
+import { Socket } from "socket.io";
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket: Socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
